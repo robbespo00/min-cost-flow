3d46aeb48dd4de832cb2502609e8fa1df289e05c
Skylong97 <96478908+Skylong97@users.noreply.github.com>
2022-01-31 11:09:43 +0100

    Updated

--- customGMRES.m
+++ customGMRES.m
@@ -6,7 +6,7 @@
 %   - c1 is a vector that corresponds to the lower part of the known term;
 %   - m is the number of iterations of the Arnoldi process;
 
-function x = customGMRES(d, E, b1, c1, m)
+function x = customGMRES(diag, E, b1, c1, m)
 
     % we create the vector b that it's composed by b1 and c1
     b = [b1; c1];
@@ -15,18 +15,12 @@ function x = customGMRES(d, E, b1, c1, m)
     % The function "arnoldiqr" returns Q, R and Qn where:
     % - Q and R are the QR decomposition of the matrix Hn;
     % - Qn is the matrix from the Arnoldi process;
-    [Q, R, Qn] = arnoldiqr(d, E, b, m);
-<<<<<<< HEAD
-    
-    % We solve the least square problem using the the submatrices of Q
-    % and R (it's cheaper)
-    y = R(1:end-1,:) / Q(1, 1:end-1)'*norm(b);
-=======
+    [Q, R, Qn] = arnoldiqr(diag, E, b, m);
 
     % We solve the least square problem using the the submatrices of Q
     % and R (it's cheaper)
-    y = R(1:end-1 \ Q(1, 1:end-1)*norm(b);
->>>>>>> 5a5e341bdea9d26ca08ac223e3f2eb7cf385dd0c
+    %y = lsqr(R(1:end-1, :), (Q(1, 1:end-1)*norm(b))', 1e-15, 5000);
+    y = R(1:end-1,:) \ (Q(1, 1:end-1)*norm(b))';
     toc
 
     % We transform the solution of the smaller least square problem into
