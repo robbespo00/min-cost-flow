62af59a4bbbf613f2425fe8457356cc8ef776fb8
Roberto <73891662+robbespo00@users.noreply.github.com>
2022-02-02 18:36:28 +0100

    Delete matrices_generator.m

--- matrices_generator.m
+++
@@ -1,7 +0,0 @@
-function [A, E, D, b, c] = matrices_generator(n, e)
-
-    E=graph_generating(n,e);
-    D = 5*rand(e,1);
-    b= 10*(rand(e,1)-(ones(e,1))/2);
-    c= 10*(rand(n,1)-(ones(n,1))/2);
-    A=[D.*eye(e) full(E)'; full(E) zeros(n,n)];
