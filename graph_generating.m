8b4c86999d2f7220e90e2d1c67ff8e1786accf4d
Roberto <73891662+robbespo00@users.noreply.github.com>
2022-02-02 18:36:24 +0100

    Delete graph_generating.m

--- graph_generating.m
+++
@@ -1,8 +0,0 @@
-function E = graph_generating(n, e)
-    
-    G=digraph(true(n), 'omitselfloops');
-    p=randperm(numedges(G), e);
-    G=digraph(G.Edges(p,:));
-    E=incidence(G);
-    E=E.*round(20*(rand(n,e)-1/2*true(n,e)));
-    
