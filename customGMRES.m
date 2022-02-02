4c9dc674a122f686abd1ddad7a8a8f7e0ed441d1
Roberto <73891662+robbespo00@users.noreply.github.com>
2022-02-02 18:35:25 +0100

    Update customGMRES.m

--- customGMRES.m
+++ customGMRES.m
@@ -1,6 +1,6 @@
 % PRECONDITIONING: 
-%   - d is a the vector corresponding to the diagonal of the matrix D;
-%   - E is a sparse matrix that corresponds to the adjacency matrix of a
+%   - diag is a the vector corresponding to the diagonal of the matrix D;
+%   - E is a sparse matrix that corresponds to the incidence matrix of a
 %       weighted directed graph;
 %   - b1 is a vector that corresponds to the upper part of the known term;
 %   - c1 is a vector that corresponds to the lower part of the known term;
@@ -11,18 +11,17 @@ function x = customGMRES(diag, E, b1, c1, m)
     % we create the vector b that it's composed by b1 and c1
     b = [b1; c1];
 
-    tic
+    
     % The function "arnoldiqr" returns Q, R and Qn where:
     % - Q and R are the QR decomposition of the matrix Hn;
     % - Qn is the matrix from the Arnoldi process;
     [Q, R, Qn] = arnoldiqr(diag, E, b, m);
 
-    % We solve the least square problem using the the submatrices of Q
-    % and R (it's cheaper)
-    %y = lsqr(R(1:end-1, :), (Q(1, 1:end-1)*norm(b))', 1e-15, 5000);
+    % We compute the solution of the system R_1*y=||b||*p_1^n (where p_1^n
+    % is defined in the report)
     y = R(1:end-1,:) \ (Q(1, 1:end-1)*norm(b))';
-    toc
+    
 
-    % We transform the solution of the smaller least square problem into
-    % the solution of the bigger one inside the Krylov subspace
+    % We compute the solution of the main problem
     x = Qn(:, 1:end-1)*y;
+
