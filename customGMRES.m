b502ababc5e9e19d220ba1d22073e89a0fbca457
Roberto <73891662+robbespo00@users.noreply.github.com>
2022-02-11 11:24:34 +0100

    Update customGMRES.m

--- customGMRES.m
+++ customGMRES.m
@@ -6,7 +6,7 @@
 %   - c1 is a vector that corresponds to the lower part of the known term;
 %   - m is the number of iterations of the Arnoldi process;
 
-function x = customGMRES(diag, E, b1, c1, m)
+function [x,q] = customGMRES(diag, E, b1, c1, m)
 
     % we create the vector b that it's composed by b1 and c1
     b = [b1; c1];
@@ -15,8 +15,11 @@ function x = customGMRES(diag, E, b1, c1, m)
     % The function "arnoldiqr" returns Q, R and Qn where:
     % - Q and R are the QR decomposition of the matrix Hn;
     % - Qn is the matrix from the Arnoldi process;
+    tic
     [Q, R, Qn] = arnoldiqr(diag, E, b, m);
-
+    
+    q=abs(Q(1,m+1));
+    
     % We compute the solution of the system R_1*y=||b||*p_1^n (where p_1^n
     % is defined in the report)
     y = R(1:end-1,:) \ (Q(1, 1:end-1)*norm(b))';
@@ -24,4 +27,5 @@ function x = customGMRES(diag, E, b1, c1, m)
 
     % We compute the solution of the main problem
     x = Qn(:, 1:end-1)*y;
+    toc
 
