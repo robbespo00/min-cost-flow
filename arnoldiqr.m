78224263d644224662aa0293c706558aa7c97607
Roberto <73891662+robbespo00@users.noreply.github.com>
2022-02-11 11:24:11 +0100

    Update arnoldiqr.m

--- arnoldiqr.m
+++ arnoldiqr.m
@@ -17,7 +17,10 @@ function [Q, R, Qn] = arnoldiqr(diag, E, q1, m)
     % intialization of the variables
     q1 = q1/norm(q1);
     Qn = zeros(n, m+1); Qn(:, 1) = q1;
-    Hn = zeros(m+1, m);
+
+    Hn1 = zeros(m,1); % main diagonal of Hn
+    Hn2 = zeros(m,1); % sub-diagonal of Hn
+    
     Q = zeros(m+1, m+1);
     R = zeros(m+1, m); 
     
@@ -26,44 +29,42 @@ function [Q, R, Qn] = arnoldiqr(diag, E, q1, m)
         % generating the new vector in the Krylov subspace (A*q_k)
         z = [ (diag.*Qn(1:d, k)) + (E'*Qn(d+1:n, k)); E*Qn(1:d, k)];  
         % we want to avoid full computation since the matrices are sparse
-        
         % compute the 3 beta_ij's of H_n
         if k==1
-            Hn(1,1)=Qn(:,1)'*z;
-            z = z - Hn(1, 1)*Qn(:, 1);
-            Hn(2,1)=norm(z);
+            Hn1(1)=Qn(:,1)'*z;
+            z = z - Hn1(1)*Qn(:, 1);
+            Hn2(1)=norm(z);
         else
-            Hn(k-1,k)=Hn(k,k-1);
-            z = z - Hn(k-1, k)*Qn(:, k-1);
-            Hn(k,k)= Qn(:, k)'*z;
-            z = z - Hn(k, k)*Qn(:, k);
-            Hn(k+1, k) = norm(z);    
+            z = z - Hn2(k-1)*Qn(:, k-1);
+            Hn1(k)= Qn(:, k)'*z;
+            z = z - Hn1(k)*Qn(:, k);
+            Hn2(k) = norm(z);
         end
         
         
         % checking for breakdown
-        if Hn(k+1, k) < 1e-10   
+        if abs(Hn2(k)) < 1e-10   
             display("BREAKDOWN HAPPENED!");
             return; 
         end 
         
         % Assigning the new Q_{k+1}
-        Qn(:, k+1) = z/Hn(k+1, k);
+        Qn(:, k+1) = z/Hn2(k);
         
         % Applying Householder reflector
         if k == 1
-            x = Hn(1:2, 1);
+            x = [Hn1(1);Hn2(1)];
             y = norm(x)*[1; 0];
             v = x - y;
             v_norm = 2/(norm(v)^2);
-            vHn = v'*Hn(1:2, 1);
+            vHn = v'*x;
             T = v.*vHn;
-            R(1:2, 1) = Hn(1:2, 1) - v_norm*T;
+            R(1:2, 1) = x - v_norm*T;
             Q(1:2, 1:2) = (eye(2)-v_norm*v.*v')';
         else 
             %Building R tilde%
-            c = Hn(k-1, k)*Q(k-1, 1:k)' + Hn(k, k)*Q(k, 1:k)';
-            x = [c(k); Hn(k+1, k)];
+            c = Hn2(k-1)*Q(k-1, 1:k)' + Hn1(k)*Q(k, 1:k)';
+            x = [c(k); Hn2(k)];
             x_norm = norm(x);
             y = x_norm*[1; 0];   
             v = x - y;
@@ -76,7 +77,8 @@ function [Q, R, Qn] = arnoldiqr(diag, E, q1, m)
             a = Q(k, k);
             p = [u zeros(k-1, 1); a 0; 0 1]; %last two rows of Q marked 
             % transponed
-            w = [v(1)^2*u v(1)*v(2)*u; v(1)^2*a v(1)*v(2)*a; v(1)*v(2) v(2)^2];
-            Q(1:k+1, k:k+1) = p-(2/(v_norm))*w;
+            gamma = [v(1)^2*u v(1)*v(2)*u; v(1)^2*a v(1)*v(2)*a; v(1)*v(2) v(2)^2];
+            Q(1:k+1, k:k+1) = p-(2/(v_norm))*gamma;
         end
     end
+    
