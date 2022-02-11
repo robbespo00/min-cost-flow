382dea382f89caab52dcdfdc2e823ff3432fa539
Roberto <73891662+robbespo00@users.noreply.github.com>
2022-02-11 11:25:03 +0100

    Update init_customGMRES.m

--- init_customGMRES.m
+++ init_customGMRES.m
@@ -1,15 +1,15 @@
-function init_customGMRES(A, b, c, D, E)
+function init_customGMRES(b, c, D, E)
 
-    % We give as input the matrix A in order to compute the residual, this
-    % does not influence the real computation time since the calculation is 
-    % outside the "tic toc" function
-    n = length(A);
 
     % given b and c we build b tilde
     b_tilde = [b; c];
     % compute the norm of b tilde
     b_norm = norm(b_tilde);
 
+    % minimum improvements to continue the algorithm
+    delta = 10^-8;
+
+    n = length(b_tilde);
 
     % Now we start to compute the customized GMRES with different number
     % of iterations of the Arnoldi process so that we show how the residual
@@ -17,7 +17,7 @@ function init_customGMRES(A, b, c, D, E)
 
     % different number of iterations of the Arnoldi process proportional to
     % the dimensions of the matrix A
-    num_iterations = [50:round(sqrt(n)):n-1, n];
+    num_iterations = [50:round(sqrt(n)):round(n/3)];
     
 
     % initialization of the variables
@@ -27,24 +27,39 @@ function init_customGMRES(A, b, c, D, E)
     t_custom = zeros(length(num_iterations),1); % time spent by customized 
     % GMRES for different number of iterations
 
-    norm_custom = zeros(length(num_iterations), 1); % initialization of the
+    residual = zeros(length(num_iterations), 1); % initialization of the
     % array of the residuals for different number of iterations
 
+    fileid = fopen("test.txt", "w");
 
     for i=1:length(num_iterations)
         tic; % start counting the time spent for one iteration
-        x_custom(:, i) = customGMRES(D, E, b, c, num_iterations(i)); 
+        [x_custom(:, i),q] = customGMRES(D, E, b, c, num_iterations(i)); 
         % apply the customized GMRES to the problem
         t_custom(i) = toc; % save the time spent for the customized GMRES 
         % where the number of iterations is num_iterations(i)
-        norm_custom(i) = norm(A*x_custom(:,i)-b_tilde)/b_norm; % compute 
+        Ax = [D.*x_custom(1:length(D),i)+E'*x_custom(length(D)+1:end,i); 
+            E*x_custom(1:length(D),i)];
+        residual(i) = norm(Ax-b_tilde)/b_norm; % compute 
         % the residual
+        scatter(q, t_custom(i), 80, "red", "filled");
+        fprintf(fileid, "m: %d\n", num_iterations(i));
+        fprintf(fileid, "q: %d\n", q);
+        fprintf(fileid, "residual: %d\n", residual(i));
+        fprintf(fileid, "t: %d\n", t_custom(i));
+        hold on
+        if i>1 && abs(residual(i)-residual(i-1)) < delta
+            fprintf("CustomGMRES stopped at the iteration %d because " + ...
+                "the difference of the residual is %d\n", i, abs(residual(i)-residual(i-1)));
+            break;
+        end 
+        
     end 
-
+    
 
     % plotting of the results obtained with customized GMRES
-    c = 1:length(num_iterations);
-    scatter(norm_custom, t_custom, 50, c,'filled');
+    c = 1:i;
+    scatter(residual(1:i), t_custom(1:i), 30, c,'filled');
     colormap(winter);
     hold on
 
@@ -54,4 +69,6 @@ function init_customGMRES(A, b, c, D, E)
     ylabel('Time (s)');
     
     % We set the values on the x axis 
-    xlim([0,0.3])
+    % xlim([0,0.3])
+
+    fclose(fileid);
