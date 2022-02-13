f0a002dc8ded8807492b73f35f48fc4a6a6c48a7
Silvio_Martinico <96478908+Skylong97@users.noreply.github.com>
2022-02-13 15:49:10 +0100

    Useless

--- graph_generator.m
+++
@@ -1,8 +0,0 @@
-function E = graph_generator(n, e, e_range)
-    
-    G=digraph(true(n), 'omitselfloops');
-    p=randperm(numedges(G), e);
-    G=digraph(G.Edges(p,:));
-    E=incidence(G);
-    E=E.*round(2*e_range*(rand(n,e)-1/2*true(n,e)));
-    
