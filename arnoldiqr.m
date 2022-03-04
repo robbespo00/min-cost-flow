2a3521960f0d2327e40f22498e952610fb6f1f81
Silvio_Martinico <96478908+Skylong97@users.noreply.github.com>
2022-03-04 16:37:38 +0100

    Update

--- arnoldiqr.m
+++ arnoldiqr.m
@@ -1,11 +1,10 @@
 function [Q, R, Qn] = arnoldiqr(diag, E, q1, m)
 
     % initialization 
-    d = length(diag); % dimensions of the matrix D
-    tau = size(E,1); % #rows of the incidence matrix E
-    n = d + tau;  % dimensions of the matrix A
+    d = length(diag);
+    tau = size(E,1);
+    n = d + tau; 
 
-    % check on the number of Arnoldi iterations
     if m <= 0
         display("Error. m must be > 0");
         return;
@@ -14,46 +13,36 @@ function [Q, R, Qn] = arnoldiqr(diag, E, q1, m)
         display("You don't need more than n iterations. m has been set to n");
     end
 
-    % intialization of the variables
     q1 = q1/norm(q1);
     Qn = zeros(n, m+1); Qn(:, 1) = q1;
-
-    Hn1 = zeros(m,1); % main diagonal of Hn
-    Hn2 = zeros(m,1); % sub-diagonal of Hn
-    
+    Hn1 = zeros(m,1); %diagonal of H_n
+    Hn2 = zeros(m,1); %subdiagonal of H_n
     Q = zeros(m+1, m+1);
     R = zeros(m+1, m); 
     
     for k = 1:m
-        
-        % generating the new vector in the Krylov subspace (A*q_k)
-        z = [ (diag.*Qn(1:d, k)) + (E'*Qn(d+1:n, k)); E*Qn(1:d, k)];  
-        % we want to avoid full computation since the matrices are sparse
-        % compute the 3 beta_ij's of H_n
+        z = [ (diag.*Qn(1:d, k)) + (E'*Qn(d+1:n, k)); E*Qn(1:d, k)]; %we want to avoid full computation since the matrices are sparse
+    
         if k==1
-            Hn1(1)=Qn(:,1)'*z;
+            Hn1(1) = Qn(:,1)'*z;
             z = z - Hn1(1)*Qn(:, 1);
-            Hn2(1)=norm(z);
+            Hn2(1) = norm(z);
         else
             z = z - Hn2(k-1)*Qn(:, k-1);
             Hn1(k)= Qn(:, k)'*z;
             z = z - Hn1(k)*Qn(:, k);
-            Hn2(k) = norm(z);
+            Hn2(k) = norm(z);    
         end
         
-        
-        % checking for breakdown
-        if abs(Hn2(k)) < 1e-10   
+        if abs(Hn2(k)) < 1e-10   %breakdown% 
             display("BREAKDOWN HAPPENED!");
             return; 
         end 
         
-        % Assigning the new Q_{k+1}
         Qn(:, k+1) = z/Hn2(k);
         
-        % Applying Householder reflector
         if k == 1
-            x = [Hn1(1);Hn2(1)];
+            x = [Hn1(1); Hn2(1)];
             y = norm(x)*[1; 0];
             v = x - y;
             v_norm = 2/(norm(v)^2);
@@ -72,13 +61,11 @@ function [Q, R, Qn] = arnoldiqr(diag, E, q1, m)
             R(1:k, k) = [c(1:k-1); x_norm];
             
             %Building Q tilde%
-            Q(k+1,k+1)=1; % build Q' marked%
+            Q(k+1,k+1)=1; %build Q' signed%
             u = Q(1:k-1, k);
             a = Q(k, k);
-            p = [u zeros(k-1, 1); a 0; 0 1]; %last two rows of Q marked 
-            % transponed
+            p = [u zeros(k-1, 1); a 0; 0 1]; %last two rows of Q signed transponed
             gamma = [v(1)^2*u v(1)*v(2)*u; v(1)^2*a v(1)*v(2)*a; v(1)*v(2) v(2)^2];
             Q(1:k+1, k:k+1) = p-(2/(v_norm))*gamma;
         end
     end
-    
