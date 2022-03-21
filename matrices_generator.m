29be2518ff4cca6d736daa3deccd1c5ffb3453ae
Roberto <73891662+robbespo00@users.noreply.github.com>
2022-03-21 09:55:23 +0100

    Delete matrices_generator.m

--- matrices_generator.m
+++
@@ -1,6 +0,0 @@
-function [D, b, c] = matrices_generator(n, e, d_range, b_range, c_range)
-
-    D = d_range*rand(e,1);
-    b= 2*b_range*(rand(e,1)-(ones(e,1))/2);
-    c= 2*c_range*(rand(n,1)-(ones(n,1))/2);
-    
