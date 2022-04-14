9b47dc53a6c01f9d6c744f63056bb583801267f2
Silvio_Martinico <96478908+SilvioM97@users.noreply.github.com>
2022-04-14 19:42:06 +0200

    Update netgenreader.m

--- netgenreader.m
+++ netgenreader.m
@@ -1,12 +1,11 @@
 function [E, b, c] = netgenreader(filename, nodes)
-
     
     M = readmatrix(filename);
     
-    c = M(1:nodes,3);
+    c = M(1:nodes,3); %taking supply vector
     
     G = digraph(M(nodes+1:end,2), M(nodes+1:end,3), 'omitselfloops');
     E = incidence(G);
     
-    
-    b = M(nodes+1:end, 4);
+    b = M(nodes+1:end, 4); %taking vector of costs
+end
