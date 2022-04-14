a4ab94d039c79920ebaac1f0d721e3f41b89e93c
Silvio_Martinico <96478908+SilvioM97@users.noreply.github.com>
2022-04-14 19:42:40 +0200

    Update residualfunc.m

--- residualfunc.m
+++ residualfunc.m
@@ -1,10 +1,13 @@
 function residualfunc(x, y)
 
-    
     y = log10(y);
+
     scatter(x,y,'blue','filled');
     hold on
+
     p = polyfit(x,y,1);
     fplot(@(t) p(1)*t+p(2), 'red', 'LineWidth', 1.7);
+
     xlabel('Number of iterations');
     ylabel('Relative residual (log)');
+end
